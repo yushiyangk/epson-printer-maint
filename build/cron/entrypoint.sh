d31c52d0aa0db6f00e1b185dbe3c6933b3e4c1d1
Yu Shiyang <yu.shiyang@gnayihs.uy>
2023-09-07 05:02:21 +0800

    Initial dockerfiles and compose.yaml

---
+++ build/cron/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Restore writeable volumes
+cp -a /etc/default-crontabs/* /etc/crontabs
+cp -a /var/spool/default-cron/* /var/spool/cron
+
+
+echo "print:epson-printer-maint@$MAIL_DOMAIN" > /etc/ssmtp/revaliases
+
+crontab="MAILTO=$MAIL_TO"
+crontab="$crontab\n* * * * Sun "'[ $(expr $(date +%W) \% '"$EPSON_PRINTER_MAINT_INTERVAL_WEEKS"') -eq 0 ]'" && /usr/bin/lp -h epson-printer-maint-cups-1 -d $EPSON_PRINTER_NAME /home/print/testprint > /dev/null && /usr/bin/lpstat -h epson-printer-maint-cups-1 -d $EPSON_PRINTER_NAME -l -t"
+echo -e "$crontab" >> /etc/crontabs/print
+
+crond -f -l 4
---
+++ build/cups/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Restore writeable volumes
+cp -a /etc/default-cups/* /etc/cups
+cp -a /run/default-cups/* /run/cups
+cp -a /var/cache/default-cups/* /var/cache/cups
+cp -a /var/spool/default-cups/* /var/spool/cups
+
+cupsd  # Start daemon
+
+case "$1" in
+	list|drivers)
+		lpinfo -m | grep escpr
+		exit 0
+		;;
+	search)
+		lpinfo -v
+		exit 0
+		;;
+	*) ;;
+esac
+
+lpadmin -p "$EPSON_PRINTER_NAME" -E -D "$EPSON_PRINTER_DESCRIPTION" -L "$EPSON_PRINTER_LOCATION" -v "$EPSON_PRINTER_URL" -m "$EPSON_PRINTER_PPD_DRIVER"
+lpstat -l -t
+
+#su cups
+
+cupsd -f  # Restart daemon in foreground
