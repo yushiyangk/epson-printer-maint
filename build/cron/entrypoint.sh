564c75fce746bf533a2cfd7a09d003d43bb09957
Yu Shiyang <yu.shiyang@gnayihs.uy>
2023-09-07 05:59:18 +0800

    Fixed crontab

--- build/cron/entrypoint.sh
+++ build/cron/entrypoint.sh
@@ -8,7 +8,7 @@ cp -a /var/spool/default-cron/* /var/spool/cron
 echo "print:epson-printer-maint@$MAIL_DOMAIN" > /etc/ssmtp/revaliases
 
 crontab="MAILTO=$MAIL_TO"
-crontab="$crontab\n* * * * Sun "'[ $(expr $(date +%W) \% '"$EPSON_PRINTER_MAINT_INTERVAL_WEEKS"') -eq 0 ]'" && /usr/bin/lp -h epson-printer-maint-cups-1 -d $EPSON_PRINTER_NAME /home/print/testprint > /dev/null && /usr/bin/lpstat -h epson-printer-maint-cups-1 -d $EPSON_PRINTER_NAME -l -t"
+crontab="$crontab\n0 $EPSON_PRINTER_MAINT_HOUR * * Sun "'[ $(expr $(date +%W) \% '"$EPSON_PRINTER_MAINT_INTERVAL_WEEKS"') -eq 0 ]'" && /usr/bin/lp -h epson-printer-maint-cups-1 -d $EPSON_PRINTER_NAME /home/print/testprint > /dev/null && /usr/bin/lpstat -h epson-printer-maint-cups-1 -d $EPSON_PRINTER_NAME -l -t"
 echo -e "$crontab" >> /etc/crontabs/print
 
 crond -f -l 4
