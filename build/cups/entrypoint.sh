d4db390729c90096c3066767818c8ab53b25c766
Yu Shiyang <yu.shiyang@gnayihs.uy>
2026-04-23 03:40:22 +0800

    Remove error messages if `entrypoint.sh` is executed again
    This might be done in order to use its included helpful subcommands.

--- build/cups/entrypoint.sh
+++ build/cups/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 # Restore writeable files
-cp -a /default/* /container
+cp -a --update=none /default/* /container  # Do not overwrite if this script is executed again
 
 # Set time zone
-ln -s /usr/share/zoneinfo/"$(cat /etc/timezone)" /container/etc/localtime
+ln -sf /usr/share/zoneinfo/"$(cat /etc/timezone)" /container/etc/localtime  # Okay to overwrite as /etc/timezone is read-only
 
 
-cupsd  # Start daemon
+# Start daemon
+cupsd
 
 case "$1" in
 	list|drivers)
@@ -24,4 +25,5 @@ esac
 lpadmin -p "$EPSON_PRINTER_NAME" -E -D "$EPSON_PRINTER_DESCRIPTION" -L "$EPSON_PRINTER_LOCATION" -v "$EPSON_PRINTER_URL" -m "$EPSON_PRINTER_PPD_DRIVER"
 lpstat -l -t
 
-cupsd -f  # Restart daemon in foreground
+# Restart daemon in foreground
+cupsd -f
